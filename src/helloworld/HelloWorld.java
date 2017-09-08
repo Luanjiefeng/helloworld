04b3089daa14cf3093e69e2c574b2e2b3d69621a
xujian <1402919445@qq.com>
2017-09-08 16:28:40 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,8 +3,8 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-		System.out.println("Hello World");
+		// TODO 寰呭啓浠ｇ爜
+		System.out.println("Hello World just so so");
 	}
 
 }
